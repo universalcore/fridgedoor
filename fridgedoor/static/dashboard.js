87cba09747f9a4eb11917662bc157626b6546074
Milton Madanda <madandat@gmail.com>
2015-03-26 20:48:30 +0200

    move templates to own files

--- fridgedoor/static/dashboard.js
+++ fridgedoor/static/dashboard.js
@@ -1,25 +1,13 @@
 gapi.analytics.ready(function() {
-
-  /**
-   * Authorize the user immediately if the user has already granted access.
-   * If no access has been created, render an authorize button inside the
-   * element with the ID "embed-api-auth-container".
-   */
   gapi.analytics.auth.authorize({
     container: 'embed-api-auth-container',
     clientid: '607034686826-asf8diukgvpl5qsrvfovf1114uqd1l6s.apps.googleusercontent.com',
   });
-
-  /**
-   * Create the first DataChart for top countries over the past 30 days.
-   * It will be rendered inside an element with the id "chart-1-container".
-   */
-
   var profiles = [
     {'label': 'India', 'id': 97544600, 'parent': 'mama'},
     {'label': 'Ghana', 'id': 97700900, 'parent': 'mama'},
     {'label': 'Kenya', 'id': 96070258, 'parent': 'mama'},
-    {'label': 'Tanzania', 'id': 96863205, 'parent': 'mama'},
+    {'label': 'Tanzania', 'id': 96068083, 'parent': 'mama'},
     {'label': 'Colombia', 'id': 96315599, 'parent': 'mama'},
     {'label': 'Philippines', 'id': 99855200, 'parent': 'mama'},
     {'label': 'Zambia', 'id': 88472004, 'parent': 'mama'},
--- fridgedoor/views.py
+++ fridgedoor/views.py
@@ -1,6 +1,11 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/base.jinja2')
-def my_view(request):
+@view_config(route_name='home', renderer='templates/index.jinja2')
+def home(request):
+    return {}
+
+
+@view_config(route_name='total_users', renderer='templates/total_users.jinja2')
+def total_users_view(request):
     return {}
